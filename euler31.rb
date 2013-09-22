7c8799882c2d295baa67f050b65bf55a4a3b63ac
discolingua <discolingua@gmail.com>
2013-09-22 18:46:26 -0400

    p31 improved

--- euler31.rb
+++ euler31.rb
@@ -1,31 +1,25 @@
 tally = 0
 
-for p200 in 0..1
-    for p100 in 0..2
-        for p50 in 0..4
-            for p20 in 0..10
-                for p10 in 0..20
-                    for p5 in 0..40
-                        for p2 in 0..100
-                            for p1 in 0..200
-                                tot = 0
-                                tot += p1
-                                tot += p2 * 2
-                                tot += p5 * 5
-                                tot += p10 * 10
-                                tot += p20 * 20
-                                tot += p50 * 50
-                                tot += p100 * 100
-                                tot += p200 * 200
-                                if (tot == 200)
-                                    puts p1.to_s + " " + p2.to_s + " " + p5.to_s + " " + p10.to_s + " " + p20.to_s + " " + p50.to_s + " " + p100.to_s + " " + p200.to_s + "\n"
-                                    tally += 1
-                                    puts tally.to_s + "\n"
-                                end
-                                if (tot > 200)
-                                    break
-                                end
-                                # puts tot.to_s + " "
+for p100 in 0..2
+    for p50 in 0..4
+        for p20 in 0..10
+            for p10 in 0..20
+                for p5 in 0..40
+                    for p2 in 0..100
+                        for p1 in 0..200
+                            tot = 0
+                            tot += p1
+                            tot += p2 * 2
+                            tot += p5 * 5
+                            tot += p10 * 10
+                            tot += p20 * 20
+                            tot += p50 * 50
+                            tot += p100 * 100
+                            if (tot == 200)
+                                tally += 1
+                            end
+                            if (tot > 200)
+                                break
                             end
                         end
                     end
@@ -34,5 +28,7 @@ for p200 in 0..1
         end
     end
 end
+
+tally += 1   # 200p coin by itself
 
 puts tally.to_s
